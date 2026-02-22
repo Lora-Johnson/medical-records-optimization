154f6f3313515a24bf71668e4a5ac125de441afa
Lora Johnson <108235672+Lora-Johnson@users.noreply.github.com>
2026-02-21 19:28:23 -0600

    Add files via upload

---
+++ patients.sql
@@ -0,0 +1,8 @@
+CREATE TABLE patients (	
+	patient_id INT AUTO_INCREMENT PRIMARY KEY,
+	birth_date DATE NOT NULL,
+	birth_gender CHAR(1) NOT NULL,
+	city VARCHAR(25) NOT NULL,
+	state VARCHAR(2) NOT NULL);
+	
+
